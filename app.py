9c462443e59703b815391eefd3a202f6d2529403
Olivier Fournier, INI-ONE-CIS-CLI-CAF <Olivier.Fournier@swisscom.com>
2019-03-13 08:10:39 +0100

    Change to master

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def index():
             envs.append({"key": env, "value": os.getenv(env)})
 
     data['message'] = "Hello from OpenShift!"
-    data['branch'] = "develop"
+    data['branch'] = "master"
     data['envVars'] = envs
 
     return jsonify(data)
