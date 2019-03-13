efa2c038f0d92a3cee61ad0dcc3cbf65f4da624c
Olivier Fournier, INI-ONE-CIS-CLI-CAF <Olivier.Fournier@swisscom.com>
2019-03-13 07:53:03 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    data = {}
+    envs = []
+
+    for env in os.environ:
+        if 'HELLO' in env:
+            envs.append({"key": env, "value": os.getenv(env)})
+
+    data['message'] = "Hello from OpenShift!"
+    data['branch'] = "develop"
+    data['envVars'] = envs
+
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
